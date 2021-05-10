b0cf25cb76a4276bbb6e7950f5925abc395507a0
maniteja <manitejaambatipelli@gmail.com>
2021-05-10 17:58:47 +0530

    commit two

---
+++ src/main/java/com/cts/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import com.cts.service.SomeService;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("World's Best Driver-selmon bhai");
+		SomeService ss=new SomeService();
+		System.out.println(ss.saySomething());
+
+	}
+
+}
